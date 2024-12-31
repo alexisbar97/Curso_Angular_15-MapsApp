a979e89099a6e4f0a89c8edfdb6949913a48ca02
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-12-31 13:21:53 -0300

    2. Video 433 Finalizado

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-screen',
   standalone: false,
-  
   templateUrl: './map-screen.component.html',
   styleUrl: './map-screen.component.css'
 })
-export class MapScreenComponent {
 
+export class MapScreenComponent {
+  constructor(
+    private placesService: PlacesService,
+  ) {}
 }
--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,8 @@ platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
 })
   .catch(err => console.error(err));
+
+if(!navigator.geolocation){
+  alert('Navegador no Soporta la Geolocalización.');
+  throw new Error('Navegador no Soporta la Geolocalización.');
+}
