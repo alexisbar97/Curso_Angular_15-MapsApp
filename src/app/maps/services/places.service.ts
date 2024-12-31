0f4e6c23a84d0201f2ef77c4c2e7fbb5e5f49d8a
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-12-31 16:10:33 -0300

    10. Video 441 Finalizado

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -6,12 +8,16 @@ import { Injectable } from '@angular/core';
 
 export class PlacesService {
   public useLocation?: [number,number];
+  public isLoadingPlaces: boolean = false;
+  public places: Feature[] = [];
 
   get isUserLocationReady(): boolean {
     return !!this.useLocation;
   }
 
-  constructor(){
+  constructor(
+    private http: HttpClient,
+  ){
     this.getUserLocation();
   }
 
@@ -28,6 +34,16 @@ export class PlacesService {
           reject();
         }
       )
+    });
+  }
+
+  getPlacesByQuery(query: string = ''){
+    this.isLoadingPlaces = true;
+
+    this.http.get<PlacesResponse>(`https://api.mapbox.com/search/geocode/v6/forward?q=${query}&limit=5&proximity=-71.40650182822317%2C-33.04615000062797&language=es&access_token=pk.eyJ1IjoiYWxleGlzYmFyOTciLCJhIjoiY200eWs2a3BnMGk5NTJsb3Bzc3pweXk1eiJ9.jcrp--kp7iAeuLp3bSprnA`)
+    .subscribe(resp => {
+      this.isLoadingPlaces = false;
+      this.places = resp.features;
     });
   }
 }
