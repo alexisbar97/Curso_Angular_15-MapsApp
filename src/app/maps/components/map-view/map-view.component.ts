1ee095e1dcf36ade1b0738b7927e4df0ef908c9f
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-12-31 14:13:28 -0300

    4. Video 435 Finalizado

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { PlacesService } from '../../services';
+import mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-view',
@@ -8,8 +9,22 @@ import { PlacesService } from '../../services';
   styleUrl: './map-view.component.css'
 })
 
-export class MapViewComponent {
+export class MapViewComponent implements AfterViewInit {
+  @ViewChild('mapDiv')
+  public mapDivElement?: ElementRef
+
   constructor(
     private placesService: PlacesService,
   ) {}
+
+  ngAfterViewInit(): void {
+    if(!this.placesService.useLocation) throw Error('No hay placesService.useLocation');
+
+    const map = new mapboxgl.Map({
+      container: this.mapDivElement?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.placesService.useLocation,
+      zoom: 14,
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGlzYmFyOTciLCJhIjoiY201Y3BoMzByM3pzbzJscHF6ZXBjdWFjbCJ9.a14cXrREPusq7ruoi-UFEA';
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
