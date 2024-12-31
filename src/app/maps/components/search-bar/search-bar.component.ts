9ca5e1fe9bc7943ff2487f74ea01c236c8b586b8
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-12-31 15:43:11 -0300

    9. Video 440 Finalizado

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   standalone: false,
-  
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
-export class SearchBarComponent {
 
+export class SearchBarComponent {
+  private debounceTimer?: NodeJS.Timeout;
+
+  constructor() {}
+
+  public onQueryChanged(query: string = '') {
+    if(this.debounceTimer) clearTimeout(this.debounceTimer);
+
+    this.debounceTimer = setTimeout(() => {
+      console.log('Mandar este query:', query);
+    }, 500);
+  }
 }
