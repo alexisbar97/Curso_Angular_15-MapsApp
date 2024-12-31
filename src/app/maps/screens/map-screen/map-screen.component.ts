d47498c3b43e405e6a28ff69d91ef0191428e097
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-12-31 13:02:00 -0300

    1. Video 432 Finalizado

---
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-screen',
+  standalone: false,
+  
+  templateUrl: './map-screen.component.html',
+  styleUrl: './map-screen.component.css'
+})
+export class MapScreenComponent {
+
+}
